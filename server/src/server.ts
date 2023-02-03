eb282b65f2cec9b33ab1719ec3b85d9290ca91f7
Heitor <heitornishimura@hotmail.com>
2023-02-03 09:29:59 -0300

    aula 3

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+    port: 5656
+}).then(() => {
+    console.log('👒👒👒 Server running on port 5656')
+})
+ 
---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface IProgressBar {
+    progress: number
+}
+
+export function ProgressBar({
+    progress
+}: IProgressBar) {
+    return (
+        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+            <div 
+                className='h-3 rounded-xl bg-violet-600'
+                role='progressbar'
+                aria-label='Progresso de hábitos completados nesse dia'
+                aria-valuenow={progress} 
+                style={{ width: `${progress}%` }}
+            />
+        </div>
+    )
+}
